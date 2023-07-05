6cd0c5dc4d48a51213b76bb3a1e44121d10e3c28
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-07-05 18:42:09 +0300

    Adjust the base layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,174 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh lpR fFf">
 
-        <q-toolbar-title>
-          Quasar App
+    <q-header bordered class="bg-white text-green">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          <span class="gt-sm">{{ $route.name }}</span>
+          <q-icon
+      class="header-icon q-pa-md lt-md"
+        name="fas fa-dove"
+        size="sm"
+        color="green"
+        />
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
+    v-model="left"
+    side="left"
+    :width="283"
+    bordered
+    show-if-above
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
+    <q-icon
+      class="header-icon q-pa-md lt-md"
+        name="fas fa-dove"
+        size="lg"
+        color="green"
         />
-      </q-list>
+
+    <q-list>
+      <q-item
+      to="/"
+      clickable
+      v-ripple
+      exact
+      >
+        <q-item-section avatar>
+          <q-icon
+          name="cottage"
+          size="md"
+          />
+
+        </q-item-section>
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+
+      <q-item
+      to="/about"
+      clickable
+      v-ripple
+      exact
+      >
+        <q-item-section avatar>
+          <q-icon
+          name="info"
+          size="md"
+          />
+
+        </q-item-section>
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+    </q-list>
+    </q-drawer>
+
+    <q-drawer
+    show-if-above
+    v-model="rightDrawerOpen"
+    side="right"
+    bordered
+    >
+
+    <q-input
+    placeholder="Search on Qwittus"
+    class="q-ma-md"
+    outlined
+    rounded
+    dense
+    >
+
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+  <q-list
+  separator
+  padding>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline
+          class="text-grey">Education</q-item-label>
+          <q-item-label
+          class="text-weight-bold">Something new!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item q-pa-md>
+        <q-item-section>
+          <q-item-label overline
+          class="text-grey">Education</q-item-label>
+          <q-item-label
+          class="text-weight-bold">Something new!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item q-pa-md>
+        <q-item-section>
+          <q-item-label overline
+          class="text-grey">Education</q-item-label>
+          <q-item-label
+          class="text-weight-bold">Something new!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
+export default {
   setup () {
     const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
       }
     }
   }
-})
+}
 </script>
+
+<!-- <style lang="sass">
+.header-icon
+position: absolute
+bottom: 0
+left: 50%
+transform: translateX(-50%)
+</style>
+-->
