1b8d75d2eea06a4f4829a6792e309feb1de16bb1
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-08-07 23:55:13 +0300

    Added new pages

---
+++ src/pages/Preferences.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">...</h4>
+    <div class="text-body1">
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageAbout'
+}
+</script>
