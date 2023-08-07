8ca4950d91f722adb1b9fba494d2b8ae608114d8
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-08-07 23:57:02 +0300

    Delete unnecesary code

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,120 +1,112 @@
 <template>
-  <q-page class="realtive-position">
+  <q-page class="relative-position">
     <q-scroll-area class="absolute full-width full-height">
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-sm">
-      <div class="col">
-        <q-input
-      bottom-slots
-      v-model="newQWeetContent"
-      class="new-qweet"
-      placeholder="Salut, la ce te gândești?"
-      counter
-      maxlength="250"
-      autogrow
-      >
-        <template v-slot:before>
-          <q-avatar size="xl">
-            <img src="https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg">
-          </q-avatar>
-        </template>
-      </q-input>
-      </div>
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewQweet"
-
-          class="q-mb-lg"
-          outline
-          rounded
-          color="primary"
-          label="Qweet"
-          unelevated
-          no-caps
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            v-model="newQweetContent"
+            class="new-qweet"
+            placeholder="What's new?"
+            maxlength="280"
+            bottom-slots
+            counter
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://www.citypng.com/public/uploads/preview/hd-profile-user-round-green-icon-symbol-transparent-png-11639594320ayr6vyoidq.png">
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewQweet"
+            :disable="!newQweetContent"
+            class="q-mb-lg"
+            color="primary"
+            label="Qweet"
+            rounded
+            unelevated
+            no-caps
           />
+        </div>
       </div>
-    </div>
 
-  <q-separator
-  class="divider"
-  size="10px"
-  color="grey-2"
-  />
-
-  <q-list separator>
-    <transition-group
-    appear
-    enter-active-class="animated fadeIn slow"
-    leave-active-class="animated fadedOut slow"
-    >
-      <q-item
-      v-for="qweet in qweets"
-      :key="qweet.id"
-      class="qweet q-py-md"
-      >
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img src="https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label class="text-subtitle1">
-            <strong>Ionut Cipleu</strong>
-            <span class="text-grey-7">
-              ionut_88
-              <br class="lt-md">&bull; {{ qweet.date | relativeDate }}
-            </span>
-          </q-item-label>
-          <q-item-label
-          class="qweet-content text-body1">
-          {{ qweet.content }}</q-item-label>
-          <div class="qweet-icons row justify-between q-mt-sm">
-      <q-btn
-        @click="toggleLiked(qweet)"
-        :color="qweet.liked ? 'pink' : 'grey'"
-        :icon="qweet.liked ? 'fas fa-heart' : 'far fa-heart'"
-        size="sm"
-        icon="far fa-heart"
-        flat
-        round
+      <q-separator
+        class="divider"
+        color="grey-2"
+        size="10px"
       />
-      <q-btn
-        color="grey"
-        size="sm"
-        icon="far fa-comment"
-        flat
-        round
-      />
-      <q-btn
-        color="grey"
-        size="sm"
-        icon="fas fa-retweet"
-        flat
-        round
-      />
-      <q-btn
-      @click="deleteQweet(qweet)"
-        color="grey"
-        size="sm"
-        icon="fas fa-trash"
-        flat
-        round
-      />
-    </div>
-        </q-item-section>
 
-        <q-item-section side top>
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn slow"
+          leave-active-class="animated fadeOut slow"
+        >
+          <q-item
+            v-for="qweet in qweets"
+            :key="qweet.id"
+            class="qweet q-py-md"
+          >
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img src="https://s.gravatar.com/avatar/ce7f3697e231df38b3ca6065848520da?s=80">
+              </q-avatar>
+            </q-item-section>
 
-        </q-item-section>
-      </q-item>
-      </transition-group>
-  </q-list>
-</q-scroll-area>
-</q-page>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>Ionut</strong>
+                <span class="text-grey-7">
+                  @ionut__cpl123
+                  <br class="lt-md">&bull; {{ qweet.date | relativeDate }}
+                </span>
+              </q-item-label>
+              <q-item-label class="qweet-content text-body1">{{ qweet.content }}</q-item-label>
+              <div class="qweet-icons row justify-between q-mt-sm">
+                <q-btn
+                  color="grey"
+                  icon="far fa-comment"
+                  size="sm"
+                  flat
+                  round
+                />
+                <q-btn
+                  color="grey"
+                  icon="fas fa-retweet"
+                  size="sm"
+                  flat
+                  round
+                />
+                <q-btn
+                  @click="toggleLiked(qweet)"
+                  :color="qweet.liked ? 'pink' : 'grey'"
+                  :icon="qweet.liked ? 'fas fa-heart' : 'far fa-heart'"
+                  size="sm"
+                  flat
+                  round
+                />
+                <q-btn
+                  @click="deleteQweet(qweet)"
+                  color="grey"
+                  icon="fas fa-trash"
+                  size="sm"
+                  flat
+                  round
+                />
+              </div>
+            </q-item-section>
+          </q-item>
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
+  </q-page>
 </template>
 
 <script>
-
+import db from 'src/boot/firebase'
 import { formatDistance } from 'date-fns'
 
 export default {
@@ -122,41 +114,71 @@ export default {
   data() {
     return {
       newQweetContent: '',
-      qweets: [
-      {
-        id: 'ID1',
-        content: 'Be your own hero, its cheaper than a movie ticket.',
-        date: 1611653238221,
+      qweets: []
+    }
+  },
+  methods: {
+    addNewQweet() {
+      let newQweet = {
+        content: this.newQweetContent,
+        date: Date.now(),
         liked: false
-        },
-        {
-        id: 'ID2',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.',
-        date: 1611653252444,
-        liked: true
-        },
-      ]
+      }
+      // this.qweets.unshift(newQweet)
+      db.collection('qweets').add(newQweet).then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id)
+      }).catch(function(error) {
+        console.error('Error adding document: ', error)
+      })
+      this.newQweetContent = ''
+    },
+    deleteQweet(qweet) {
+      db.collection('qweets').doc(qweet.id).delete().then(function() {
+        console.log('Document successfully deleted!');
+      }).catch(function(error) {
+        console.error('Error removing document: ', error);
+      })
+    },
+    toggleLiked(qweet) {
+      db.collection('qweets').doc(qweet.id).update({
+        liked: !qweet.liked
+      })
+      .then(function() {
+        console.log('Document successfully updated!')
+      })
+      .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error('Error updating document: ', error)
+      })
     }
-},
-methods: {
-  addNewQweet() {
-    let newQweet = {
-      content: this.newQweetContent,
-      date: Date.now()
-    }
-    this.qweets.unshift(newQweet)
   },
-  deleteQweet(qweet) {
-    let dateToDelete = qweet.date
-    let index = this. qweets.findIndex(qweet => qweet.date === dateToDelete)
-    this.qweets.splice(index, 1)
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date())
+    }
+  },
+  mounted() {
+    db.collection('qweets').orderBy('date').onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        let qweetChange = change.doc.data()
+        qweetChange.id = change.doc.id
+        if (change.type === 'added') {
+          console.log('New qweet: ', qweetChange)
+          this.qweets.unshift(qweetChange)
+        }
+        if (change.type === 'modified') {
+          console.log('Modified qweet: ', qweetChange)
+          let index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+          Object.assign(this.qweets[index], qweetChange)
+        }
+        if (change.type === 'removed') {
+          console.log('Removed qweet: ', qweetChange)
+          let index = this.qweets.findIndex(qweet => qweet.id === qweetChange.id)
+          this.qweets.splice(index, 1)
+        }
+      })
+    })
   }
-},
-filters: {
-  relativeDate(value) {
-return formatDistance(value, new Date())
-    }
-  },
 }
 </script>
 
@@ -169,8 +191,10 @@ return formatDistance(value, new Date())
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
-  .qweet-content
-    white-space: pre-line
-    .qweeti-icons
-      margin-left: -5px
+.qweet:not(:first-child)
+  border-top: 1px solid rgba(0, 0, 0, 0.12)
+.qweet-content
+  white-space: pre-line
+.qweet-icons
+  margin-left: -5px
 </style>
