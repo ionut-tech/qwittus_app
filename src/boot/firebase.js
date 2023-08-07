e3d90c8f4c4ea61d735a5f1ac6d170ce529687ec
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-08-07 23:55:48 +0300

    Modify firebase file

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,2 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD6u7iCCD89yes8Fyt2Crid00Rw4f3GA8",
+  authDomain: "qwettus.firebaseapp.com",
+  projectId: "qwettus",
+  storageBucket: "qwettus.appspot.com",
+  messagingSenderId: "267500450961",
+  appId: "1:267500450961:web:82bdda63e357fbf28fbb30"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore()
+
+export default db
