1ebd6ae86c9469173159584f6d24a73170a18409
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-08-15 10:48:12 +0300

    Create new notification design

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -1,7 +1,52 @@
 <template>
   <q-page class="q-pa-lg">
-    <h4 class="q-mt-none q-mb-md text-weight-bold">...</h4>
+    <h4 class="q-mt-none q-mb-md text-weight-bold"></h4>
     <div class="text-body1">
+      <q-list bordered class="rounded-borders" style="max-width: 4300px">
+      <q-item-label header>From friends</q-item-label>
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">Brunch this weekend?</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">Janet</span>
+            -- Uploaded a new Qweet
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+      <q-separator inset="item" />
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">Linear Project</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">John</span>
+            -- Respond to your qweet
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 h ago
+        </q-item-section>
+      </q-item>
+    </q-list>
     </div>
   </q-page>
 </template>
