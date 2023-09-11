9b0ff33e5ef480c6a201e5962b4a43b5ee649ab7
ionut-tech <111152241+ionut-tech@users.noreply.github.com>
2023-09-11 23:04:36 +0300

    big update for preference page

--- src/pages/Preferences.vue
+++ src/pages/Preferences.vue
@@ -1,7 +1,98 @@
 <template>
   <q-page class="q-pa-lg">
-    <h4 class="q-mt-none q-mb-md text-weight-bold">...</h4>
+    <h4 class="q-mt-none q-mb-md text-weight-bold"></h4>
     <div class="text-body1">
+      <div class="q-pa-md">
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Food.vue">
+          <q-img
+          src="https://t4.ftcdn.net/jpg/03/32/75/39/360_F_332753934_tBacXEgxnVplFBRyKbCif49jh0Wz89ns.jpg">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Food
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Tech.vue">
+          <q-img
+          src="https://www.gazettebourgogne.fr/thumbs/1368%C3%971026/articles/2023/06/AdobeStock-602373178.jpeg">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Tech
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Cars.vue">
+          <q-img
+          src="https://cfx-wp-images.imgix.net/2020/01/Types-of-Cars-Collage.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=4fea469b66200f7b621d9dc3c845f04e">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Cars
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/History.vue">
+          <q-img
+          src="https://im.indiatimes.in/content/2023/Jun/today-in-history-6_647cc5c17112a.jpg?w=820&h=540&cc=1">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            History
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Education.vue">
+          <q-img
+          src="https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2015/12/01/Pictures/_c34102da-9849-11e5-b4f4-1b7a09ed2cea.jpg">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Education
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Travel.vue">
+          <q-img
+          src="https://motuslearning.com/wp-content/uploads/2022/01/shutterstock-1041475570-1.jpg">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Travel
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Sports.vue">
+          <q-img
+          src="https://decathlonbailemunna.simplybook.it/uploads/decathlonbailemunna/image_files/preview/6c51654f269cc745da7dc0ed6b60abf4.jpg">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Sports
+          </div>
+          </q-img>
+        </a>
+      </div>
+
+      <div class="col-6">
+        <a href="src/pages/components/news pg/Gaming.vue">
+          <q-img
+          src="https://static.independent.co.uk/2022/03/02/15/best%20console.jpg?quality=75&width=1200&auto=webp">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            Gaming
+          </div>
+          </q-img>
+        </a>
+      </div>
+    </div>
+  </div>
     </div>
   </q-page>
 </template>
